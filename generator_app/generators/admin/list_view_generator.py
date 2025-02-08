0abcaa1bbb89c5318a3aa8ae9a0ed92773c746c7
Omar Gawdat <omargawdaat@gmail.com>
2025-02-08 10:13:22 +0200

    feat: working smoothly

--- generator_app/generators/admin/list_view_generator.py
+++ generator_app/generators/admin/list_view_generator.py
@@ -4,7 +4,8 @@ from ..base import BaseGenerator
 class ListViewGenerator(BaseGenerator):
     def generate(self, fields: list) -> None:
         content = f'''class {self.model_name_capital}ListView:
-    list_display = ()
+    list_display =  ("display_header",)
+    list_editable = ()
     list_filter = ()
     date_hierarchy = None
     list_per_page = 50
