fb7df6a13aa6d91a3416e9a89e04107c1c9ddc74
Omar Gawdat <omargawdaat@gmail.com>
2025-02-12 12:44:05 +0200

    feat: add testing model

--- AppGenerator/settings.py
+++ AppGenerator/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_model_suite',
+    'app'
 ]
 
 MIDDLEWARE = [
---
+++ app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class TestModel(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class TestModelRelated(models.Model):
+    test_model = models.ForeignKey(TestModel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
