30b41baee369e406f00cbff25d8196771270ce6c
Omar Gawdat <omargawdaat@gmail.com>
2025-02-12 13:50:46 +0200

    fix: generate auto generated models

--- app/models.py
+++ app/models.py
@@ -9,3 +9,8 @@ class TestModelRelated(models.Model):
     test_model = models.ForeignKey(TestModel, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
 
+
+class UnRelatedModel(models.Model):
+    name = models.CharField(max_length=100)
+    test_model = models.ForeignKey(TestModel, on_delete=models.CASCADE)
+    test_model_related = models.ForeignKey(TestModelRelated, on_delete=models.CASCADE)
--- django_model_suite/generators/model_utils.py
+++ django_model_suite/generators/model_utils.py
@@ -4,10 +4,13 @@ from django.apps import apps
 
 
 def get_model_fields(app_name: str, model_name: str) -> list:
-    """Get all fields from a model, including relations."""
+    """Get direct fields and foreign keys from a model."""
     model = apps.get_model(app_name, model_name)
-    return [field.name for field in model._meta.get_fields()]
-
+    return [
+        field.name
+        for field in model._meta.get_fields()
+        if not field.auto_created
+    ]
 def ensure_package(path: str) -> None:
     os.makedirs(path, exist_ok=True)
     init_file = os.path.join(path, "__init__.py")
